9adf1e160b45c30046c831dd630765d754ee0b94
juanrodriguezmt <91926824+juanrodriguezmt@users.noreply.github.com>
2021-12-11 12:53:57 -0500

    Add files via upload

---
+++ src/main/java/Reto1/UserCrudRepository.java
@@ -0,0 +1,20 @@
+package Reto1;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import Reto1.User;
+
+
+public interface UserCrudRepository extends CrudRepository<User, Integer>{
+	
+	@Query("Select u FROM User u where u.email=?1")
+	public Optional<User> validateEmail(String email);
+
+	@Query("Select u FROM User u where u.email=?1 and u.password=?2")
+	public Optional<User> validateCredentials(String email, String password);
+		
+}
---
+++ src/main/java/Reto1/UserRepository.java
@@ -0,0 +1,38 @@
+package Reto1;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+
+
+@Repository
+public class UserRepository {
+	
+	@Autowired
+	UserCrudRepository userCrud;
+	
+	//1. Listar todos los usuarios
+	public List<User> getAll(){
+	        return (List<User>) userCrud.findAll();
+	}
+	//2. Buscar usuario en especifico
+	public Optional<User> getUser(int id){
+        return userCrud.findById(id);
+    }
+	//3. Registrar usuarios
+	public User save(User p){
+	        return userCrud.save(p);
+	}
+	 //3. Validar si existe email
+	public Optional<User> validateEmail(String userEmail){
+		return userCrud.validateEmail(userEmail);
+	}
+	 //3. Validar credenciales de acceso
+	 public Optional<User> validateCredentials(String userEmail, String userPassword){
+		return userCrud.validateCredentials(userEmail, userPassword);
+	}
+	 
+}
